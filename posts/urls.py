f94ee6d4dabe0939874314309374278de87692b3
johnny fumbwe <johnnyfumbwe@hotmail.com>
2025-08-25 15:01:52 +0200

    Add CRUD to post modal

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from .views import HomePageView
 from .views import PostDetailView
+from .views import PostCreateView
+from .views import PostUpdateView
+from .views import PostDeleteView
 
 urlpatterns = [
     
+    path("post/new/", PostCreateView.as_view(), name="post_new"),
     path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
+    path("post/<int:pk>/edit/", PostUpdateView.as_view(), name="post_edit"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
     path("", HomePageView.as_view(), name="home"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from .models import Post
+from django.urls import reverse_lazy
 # Create your views here.
 
 class HomePageView(ListView):
@@ -11,3 +13,20 @@ class HomePageView(ListView):
 class PostDetailView(DetailView):
     queryset = Post.objects.all()
     template_name = "post_detail.html"
+
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "body", "author"]
+    
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body", "author"]
+    
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
