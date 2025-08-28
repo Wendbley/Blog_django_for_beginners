78006519f1c6cd15bd26bc7185d678c9e3cae1bd
johnny fumbwe <johnnyfumbwe@hotmail.com>
2025-08-28 11:53:03 +0200

    Fix the allowed-hots for onrender.com

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-=j@pcr&jj5z+xiajx5=+#zs-t^j3)6(dcbp)@yz7z#*amg%$uq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".render.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [".onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
