e73d82fcf71a3687549c4350e5ae30398509fddb
johnny fumbwe <johnnyfumbwe@hotmail.com>
2025-08-28 11:49:21 +0200

    Added SECRET_KET to settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-import os
+
 from pathlib import Path
-from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-if not SECRET_KEY:
-    raise ImproperlyConfigured("The SECRET_KEY setting must not be empty.")
-
+SECRET_KEY = "django-insecure-=j@pcr&jj5z+xiajx5=+#zs-t^j3)6(dcbp)@yz7z#*amg%$uq"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
