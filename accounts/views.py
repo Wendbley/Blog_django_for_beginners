1c229f14631be5ea3ef98b3c5095a9c2fe38bf7a
johnny fumbwe <johnnyfumbwe@hotmail.com>
2025-08-29 07:55:13 +0200

    Updated forms (signup) - Registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,9 @@
 # accounts/views.py
 from django.urls import reverse_lazy
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 from django.views.generic import CreateView
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.urls import reverse
+from django.conf import settings
 
-# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
